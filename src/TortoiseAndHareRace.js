d4ef803275231d775f89a452513266c56950ee66
Thabani Tembe <ThabaniT@SAHOMELOANS.COM>
2018-06-06 08:12:49 +0200

    refracted code

--- src/TortoiseAndHareRace.js
+++ src/TortoiseAndHareRace.js
@@ -1,29 +1,39 @@
 function TortoiseAndHareRace() {
 
     function race(speed1, speed2, distance, sleep, getup) {
-        let startPointtoGetUp = distance - getup;
+
+        let minutes = getMinutesSlept(distance,getup,speed2);
+        let winner = getWinner(speed1, speed2, distance, sleep, getup);
+
+        return `${winner} The hare is sleeping for ${minutes} minutes.`
+
+    }
+
+    function getWinner(speed1, speed2, distance, sleep, getup) {
+
         let tortoiseTimeToFinish = ((getup) / speed2);
         let hareTimeToFinish = ((distance - sleep) / speed1);
-        let minutes = ((startPointtoGetUp) - (speed2 * 5)) / speed2;
-        let winnder = ""
-        if (tortoiseFinishesFirst()) {
-            winner = "The tortoise won the race."
+
+        if (hareFinishesFirst()) {
+            return `The hare won the race.`
         }
-        else if (hareFinishesFirst()) {
-            winner = "The hare won the race."
+        else if (tortoiseFinishesFirst()) {
+            return `The tortoise won the race.`
         }
-        else {
-            winner = "The hare and the tortoise tied."
-        }
-        return "" + winner + " The hare is sleeping for " + minutes + " minutes."
+        return `The hare and the tortoise tied.`
 
         function hareFinishesFirst() {
             return hareTimeToFinish < tortoiseTimeToFinish;
         }
-
+    
         function tortoiseFinishesFirst() {
             return tortoiseTimeToFinish < hareTimeToFinish;
         }
+    }
+
+    function getMinutesSlept(distance,getup,speed2){
+        let startPointtoGetUp = distance - getup;
+        return ((startPointtoGetUp) - (speed2 * 5)) / speed2
     }
 
     return {
